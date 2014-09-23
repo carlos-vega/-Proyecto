a3505a847dc9a904b55b93796591db4603036ef9
Carlos Vega <j.carlos.vega.galindo@gmail.com>
2014-09-23 09:50:17 -0500

    Se agrego clase Quitar

--- Persona.java
+++ Persona.java
@@ -4,9 +4,16 @@ class Persona{
 	int edad
 	int nivel;
 	
+<<<<<<< Updated upstream
 	int contarAlumnos()
 	{
 	
+	}	
+=======
+	public Persona(String nombre, String paterno, int edad){
+		this.nombre=nombre;
+		this.paterno=paterno;
+		this.edad=edad;
 	}
-	
+>>>>>>> Stashed changes
 }
